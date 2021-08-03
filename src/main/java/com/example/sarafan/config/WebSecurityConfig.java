c55543b37aa1f2c6043100b0098e9966760412a0
sanch <alexander.romanko.job@gmail.com>
2021-08-03 21:47:11 +0300

    added Vue Router

--- src/main/java/com/example/sarafan/config/WebSecurityConfig.java
+++ src/main/java/com/example/sarafan/config/WebSecurityConfig.java
@@ -2,9 +2,6 @@ package com.example.sarafan.config;
 
 import com.example.sarafan.entity.User;
 import com.example.sarafan.repository.UserRepository;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,16 +11,12 @@ import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
 
 @Configuration
 @EnableWebSecurity
-//@EnableOAuth2Sso
 public class WebSecurityConfig<PrincipalExtractor> extends WebSecurityConfigurerAdapter {
 
     private final UserRepository userRepository;
